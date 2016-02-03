847fe786ae62cb58b0ef2ec6a0338284c67c4841
Daniel Holmgren <dansdebate@gmail.com>
2016-02-03 14:30:47 -0500

    express installed and routes working

--- server/main.js
+++ server/main.js
@@ -1,5 +1,15 @@
 var http = require('http');
+//mongo
 var mongojs = require('mongojs');
 var uri = 'mongodb://admin:admin@ds055495.mongolab.com:55495/phil-games';
 var collections = ['demo'];
 var db = mongojs(uri, collections);
+//express
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+	res.send('Hello World');
+});
+
+app.listen(3000);
