2aa901649c0166e2783b912b438d327bb638f569
Daniel Holmgren <dansdebate@gmail.com>
2016-04-10 16:42:55 -0400

    small changes, merging

--- client/js/application.js
+++ client/js/application.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
 	} else{
 		user = new User();
 	}
-	
+
 	var userJson = {
 		categories: [{plural:'cats'}, {plural:'dogs'}, {plural:'rain'}],
 		premises: ["some not 2 are 3", "no 1 are 3 and not 2", "all 3 are not 1"],
